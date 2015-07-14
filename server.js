9fc39598ad66e6feaf8a1517424e0815bbb6c27f
spandanb <spandan.777@gmail.com>
2015-07-14 11:39:42 -0400

    Added basic server

---
+++ server.js
@@ -0,0 +1,43 @@
+/******** Dependencies ************/
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+
+/******** Database Config ************/
+//require('./models/post');
+//var Post = mongoose.model('Post');
+mongoose.connect('mongodb://localhost/deep');
+
+/******** Configuration  **********/
+var app = express();
+
+app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'www')));
+
+
+/******** Routes  **********/
+//For debugging
+app.all('*', function(req, res, next){
+    var output = req.method + " " + req.url;
+    if(!!req.body && Object.keys(req.body).length){
+        output += ", " + JSON.stringify(req.body);
+    }
+    console.log(output);
+    next();
+});
+
+//Run app
+var server = app.listen(3000, function () {
+
+      var host = server.address().address;
+      var port = server.address().port;
+
+      console.log('Example app listening at http://%s:%s', host, port);
+
+});
