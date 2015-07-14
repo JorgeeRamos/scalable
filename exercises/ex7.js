af5198f8c1d0b7fda8fad87b965d87400d78d833
spandanb <spandan.777@gmail.com>
2015-07-13 20:24:53 -0400

    Added exercises and checker

---
+++ exercises/ex7.js
@@ -0,0 +1,15 @@
+////////////********////////////
+//////////Exercise 7 ///////////
+////////////********////////////
+
+//Write a function that takes an array and 
+//returns the index of the first element of 
+//first sequence of matching subsequence 
+//return -1 if no match is found
+//you can assume the subsequence is of length = 3
+//you can write addition functions if needed
+
+var answer = -1;
+//Inser code here
+
+console.log(answer);
